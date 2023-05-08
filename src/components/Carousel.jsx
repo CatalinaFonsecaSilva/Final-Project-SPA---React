8619d08e64a3d83b6f1c65dc17a80f23e8a00d47
CatalinaFonsecaSilva <catalina.fonsecasilva@dci.education>
2023-05-08 09:56:32 +0200

    fertig carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -17,12 +17,38 @@ const Carousel = () => {
 
     return (
         <section className="carousel-container">
-            <div className={index == 0 ? "active-slide" : "hidde"}> 0 </div>
-            <div className={index == 1 ? "active-slide" : "hidde"}> 1 </div>
-            <div className={index == 2 ? "active-slide" : "hidde"}> 2 </div>
-            <div className={index == 3 ? "active-slide" : "hidde"}> 3 </div>
-            <button onClick={handleBackwards}>back</button>
-            <button onClick={handleForwards}>forward</button>
+            <div className={index == 0 ? "active-slide" : "hidde"}>
+                {" "}
+                <img
+                    src="src/assets/images/baby_kit.jpg"
+                    alt="knitted baby kit"
+                />
+            </div>
+            <div className={index == 1 ? "active-slide" : "hidde"}>
+                {" "}
+                <img
+                    src="src/assets/images/headband.jpg"
+                    alt="knitted headband"
+                />
+            </div>
+            <div className={index == 2 ? "active-slide" : "hidde"}>
+                {" "}
+                <img
+                    src="src/assets/images/pullover_M.jpeg"
+                    alt="knitted pullover"
+                />{" "}
+            </div>
+            <div className={index == 3 ? "active-slide" : "hidde"}>
+                {" "}
+                <img
+                    src="src/assets/images/socken_M.jpg"
+                    alt="knitted socken"
+                />{" "}
+            </div>
+            <div className="carousel-buttons">
+                <button onClick={handleBackwards}> ⬅️ Previous </button>
+                <button onClick={handleForwards}> Next ➡️</button>
+            </div>
         </section>
     );
 };
