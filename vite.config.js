1d90843d2cdc7732d0f86901d801b808572af92c
CatalinaFonsecaSilva <catalina.fonsecasilva@dci.education>
2024-07-05 18:23:00 +0200

    Update base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://github.com/CatalinaF-S/Final-Project-SPA---React",
+    base: "/Final-Project-SPA---React/",
 });
