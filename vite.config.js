1a37488c46c22c7e9b67e5408837a4ffb1f3bbbd
CatalinaFonsecaSilva <catalina.fonsecasilva@dci.education>
2024-07-05 22:57:18 +0200

    Add vite plugin node

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePluginNode } from "vite-plugin-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/Final-Project-SPA---React/",
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePluginNode({
+            // Especifica las opciones de plugin aquí
+            // En general, deberías usar la configuración predeterminada
+        }),
+    ],
 });
