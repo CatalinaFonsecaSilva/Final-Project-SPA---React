dd4f4c29f548b65be29992d4fa11fa6922eaf361
CatalinaFonsecaSilva <catalina.fonsecasilva@dci.education>
2024-07-05 23:17:32 +0200

    Delete vite plugin node

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePluginNode } from "vite-plugin-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/Final-Project-SPA---React/",
-    plugins: [
-        react(),
-        VitePluginNode({
-            // Especifica las opciones de plugin aquí
-            // En general, deberías usar la configuración predeterminada
-        }),
-    ],
+    plugins: [react()],
 });
