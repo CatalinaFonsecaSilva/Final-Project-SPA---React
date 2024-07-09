60f02b7165fc4bdc7ddde0abb24c8e8c40f8a0f1
CatalinaFonsecaSilva <catalina.fonsecasilva@dci.education>
2024-07-09 22:38:20 +0200

    Undo public thing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <h2>About me</h2>
 
             <img
-                src="public/images/yarn-hero2.jpeg"
+                src="/images/yarn-hero2.jpeg"
                 alt="image person with yarn on arms"
             />
 
