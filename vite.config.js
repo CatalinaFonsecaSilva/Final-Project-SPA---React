fb22c1f8a224da7b65829063185600bddc2b64b1
CatalinaFonsecaSilva <catalina.fonsecasilva@dci.education>
2024-07-11 21:33:59 +0200

    Delete commonjs plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import commonjs from "@rollup/plugin-commonjs";
+// import commonjs from "@rollup/plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/Final-Project-SPA-React/",
-    plugins: [react(), commonjs()],
+    plugins: [react()],
     build: {
         commonjsOptions: {
             transformMixedEsModules: true,
