91b7fa857851342079135335e097f364e74e2959
CatalinaFonsecaSilva <catalina.fonsecasilva@dci.education>
2024-08-23 11:45:52 +0200

    Change base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/Final-Project-SPA-React/",
+    base: "./",
     plugins: [react()],
     build: {
         commonjsOptions: {
